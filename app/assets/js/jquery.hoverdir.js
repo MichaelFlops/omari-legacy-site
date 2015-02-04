eb82d823e71f32b74ce409470b87ae1303f009ce
pndpo <peterdepaulo@gmail.com>
2015-02-03 21:33:27 -0800

    remove extra script tags

--- app/assets/js/jquery.hoverdir.js
+++ app/assets/js/jquery.hoverdir.js
@@ -4,16 +4,16 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2012, Codrops
  * http://www.codrops.com
  */
 ;( function( $, window, undefined ) {
-	
+
 	'use strict';
 
 	$.HoverDir = function( options, element ) {
-		
+
 		this.$el = $( element );
 		this._init( options );
 
@@ -30,7 +30,7 @@
 	$.HoverDir.prototype = {
 
 		_init : function( options ) {
-			
+
 			// options
 			this.options = $.extend( true, {}, $.HoverDir.defaults, options );
 			// transition properties
@@ -44,23 +44,23 @@
 		_loadEvents : function() {
 
 			var self = this;
-			
+
 			this.$el.on( 'mouseenter.hoverdir, mouseleave.hoverdir', function( event ) {
-				
+
 				var $el = $( this ),
 					$hoverElem = $el.find( 'div' ),
 					direction = self._getDir( $el, { x : event.pageX, y : event.pageY } ),
 					styleCSS = self._getStyle( direction );
-				
+
 				if( event.type === 'mouseenter' ) {
-					
+
 					$hoverElem.hide().css( styleCSS.from );
 					clearTimeout( self.tmhover );
 
 					self.tmhover = setTimeout( function() {
-						
+
 						$hoverElem.show( 0, function() {
-							
+
 							var $el = $( this );
 							if( self.support ) {
 								$el.css( 'transition', self.transitionProp );
@@ -68,27 +68,27 @@
 							self._applyAnimation( $el, styleCSS.to, self.options.speed );
 
 						} );
-						
-					
+
+
 					}, self.options.hoverDelay );
-					
+
 				}
 				else {
-				
+
 					if( self.support ) {
 						$hoverElem.css( 'transition', self.transitionProp );
 					}
 					clearTimeout( self.tmhover );
 					self._applyAnimation( $hoverElem, styleCSS.from, self.options.speed );
-					
+
 				}
-					
+
 			} );
 
 		},
 		// credits : http://stackoverflow.com/a/3647634
 		_getDir : function( $el, coordinates ) {
-			
+
 			// the width and height of the current div
 			var w = $el.width(),
 				h = $el.height(),
@@ -97,19 +97,19 @@
 				// gets the x value relative to the center of the DIV and "normalize" it
 				x = ( coordinates.x - $el.offset().left - ( w/2 )) * ( w > h ? ( h/w ) : 1 ),
 				y = ( coordinates.y - $el.offset().top  - ( h/2 )) * ( h > w ? ( w/h ) : 1 ),
-			
+
 				// the angle and the direction from where the mouse came in/went out clockwise (TRBL=0123);
 				// first calculate the angle of the point,
 				// add 180 deg to get rid of the negative values
 				// divide by 90 to get the quadrant
 				// add 3 and do a modulo by 4  to shift the quadrants to a proper clockwise TRBL (top/right/bottom/left) **/
 				direction = Math.round( ( ( ( Math.atan2(y, x) * (180 / Math.PI) ) + 180 ) / 90 ) + 3 ) % 4;
-			
+
 			return direction;
-			
+
 		},
 		_getStyle : function( direction ) {
-			
+
 			var fromStyle, toStyle,
 				slideFromTop = { left : '0px', top : '-100%' },
 				slideFromBottom = { left : '0px', top : '100%' },
@@ -117,7 +117,7 @@
 				slideFromRight = { left : '100%', top : '0px' },
 				slideTop = { top : '0px' },
 				slideLeft = { left : '0px' };
-			
+
 			switch( direction ) {
 				case 0:
 					// from top
@@ -140,9 +140,9 @@
 					toStyle = slideLeft;
 					break;
 			};
-			
+
 			return { from : fromStyle, to : toStyle };
-					
+
 		},
 		// apply a transition or fallback to jquery animate based on Modernizr.csstransitions support
 		_applyAnimation : function( el, styleCSS, speed ) {
@@ -153,68 +153,68 @@
 		},
 
 	};
-	
+
 	var logError = function( message ) {
 
 		if ( window.console ) {
 
 			window.console.error( message );
-		
+
 		}
 
 	};
-	
+
 	$.fn.hoverdir = function( options ) {
 
 		var instance = $.data( this, 'hoverdir' );
-		
+
 		if ( typeof options === 'string' ) {
-			
+
 			var args = Array.prototype.slice.call( arguments, 1 );
-			
+
 			this.each(function() {
-			
+
 				if ( !instance ) {
 
 					logError( "cannot call methods on hoverdir prior to initialization; " +
 					"attempted to call method '" + options + "'" );
 					return;
-				
+
 				}
-				
+
 				if ( !$.isFunction( instance[options] ) || options.charAt(0) === "_" ) {
 
 					logError( "no such method '" + options + "' for hoverdir instance" );
 					return;
-				
+
 				}
-				
+
 				instance[ options ].apply( instance, args );
-			
+
 			});
-		
-		} 
+
+		}
 		else {
-		
+
 			this.each(function() {
-				
+
 				if ( instance ) {
 
 					instance._init();
-				
+
 				}
 				else {
 
 					instance = $.data( this, 'hoverdir', new $.HoverDir( options, this ) );
-				
+
 				}
 
 			});
-		
+
 		}
-		
+
 		return instance;
-		
+
 	};
-	
+
 } )( jQuery, window );
