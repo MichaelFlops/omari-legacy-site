901735bf1d7af0a571f53b57bbbea789ef1ca6df
Tyler Smith <tylsmith@gmail.com>
2015-02-26 18:41:11 -0800

    video carousel button works (no videos yet)

---
+++ app/assets/js/random_video.js
@@ -0,0 +1,26 @@
+
+$(function() {
+  
+  var videos = [
+      'https://s3-us-west-1.amazonaws.com/firepenguin/michael+sounds.MOV',
+      'https://dl.dropboxusercontent.com/u/57909785/work.mp4'
+  ];
+
+  $("#carousel-button").click(function(){
+    newVideo();
+  })
+
+  var newVideo = function(){
+    if($("#carousel-link"))
+      $("#carousel-link").remove();
+
+    var link = videos[Math.floor(Math.random() * videos.length)];
+
+    $("#video-carousel").append("<source id=\"carousel-link\" src=\"" + link + "\" type=\"video/mp4\" />");
+    $("#video-carousel").load();
+  }
+
+  newVideo();
+
+});
+
